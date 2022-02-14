1474117fbeaf17b8ffb11e0caa18f73a446a8107
Bruno Coelho da Fonseca <87996885+Fonseca-BrunoC@users.noreply.github.com>
2022-02-14 15:28:39 -0300

    Add files via upload

--- tela.py
+++ tela.py
@@ -1,7 +1,14 @@
 cod_helper = '''
 MDTextField:
-    hint_text: 'Insira o código da moeda desejada:'
-    pos_hint: {'center_x':0.5, 'center_y':0.5}
+    hint_text: 'Insira o código da moeda que deseja converter'
+    pos_hint: {'center_x':0.5, 'center_y':0.6}
+    size_hint_x: None
+    width: 300
+'''
+cod2_helper = '''
+MDTextField:
+    hint_text: 'Insira o código da moeda desejada'
+    pos_hint: {'center_x': 0.5, 'center_y': 0.4}
     size_hint_x: None
     width: 300
 '''
